b40b66a65fde139a418c43d61a314fee591aaf12
Fridah-Wothay <wothayafridahkamau@gmail.com>
2024-03-25 08:26:47 +0300

    add file

---
+++ src/Main.kt
@@ -0,0 +1,33 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main(){
+
+println(number: <Array>: Int):
+ String("Barne bakes brown bagels and bans")
+    println(words:String):Boolean
+}
+fun number(number: Int):Int {
+    var number = arrayOf(20, 30, 40, 50, 60)
+    println(number.sum())
+    println(number.count())
+    println(number.average())
+   return (number.sum())
+}
+fun words(words:String){
+    var word = "Barne bakes brown bagels and  bans"
+    println(word.slice "b")
+
+}
+fun isPalindrome(word:String):Boolean {
+
+    var names = "madam, wow,kayak"
+    return (names==true){
+
+        or else
+    }
+        false
+
+
+
+
+}
